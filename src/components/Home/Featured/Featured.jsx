fdc5343e0fa4373c1d67626e7bebd30e50460c3c
Andrey Lo <loandrey7@gmail.com>
2026-05-05 12:19:43 +0300

    coffeestyle v1.8

--- src/components/Home/Featured/Featured.jsx
+++ src/components/Home/Featured/Featured.jsx
@@ -1,12 +1,40 @@
-import React from "react";                                                                         
-import "./Featured.css";                                                     
-import "./FeaturedResponsive.css";                                            
+import React from "react";
+import { Link } from "react-router-dom";
+import featured_1 from "./images/featured-1.webp";
+import featured_2 from "./images/featured-2.webp";
+import "./Featured.css";
+import "./FeaturedResponsive.css";
 
 const Featured = () => {
 	return (
-		<section className="featured">                                                                     
+		<section className="featured">
 			<div className="container">
-				<h1>Section Featured</h1>	
+				<div className="featured-content">
+					<div className="featured-title-block">
+						<div className="featured-line"></div>
+						<p className="featured-title">Featured Mugs</p>
+						<div className="featured-line"></div>
+					</div>
+					<div className="featured-block">
+						<div className="featured-card">
+							<img src={featured_1} alt="Photo" className="featured-img" />
+							<Link className="featured-link" to="#!">
+								<p className="featured-text">Pink Premium Ceramic</p>
+								<p className="featured-price">$ 99.00 USD</p>
+							</Link>
+						</div>
+						<div className="featured-card">
+							<img src={featured_2} alt="Photo" className="featured-img" />
+							<Link className="featured-link" to="#!">
+								<p className="featured-text">Golden Designers Mug</p>
+								<div className="featured-price-block">
+									<p className="featured-price-2">$50.00</p>
+									<p className="features-price-no">$69.00 USD</p>
+								</div>
+							</Link>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
