cc58e48dc0aa9aa0d99b8a0680fead64c098e636
Andrey Lo <loandrey7@gmail.com>
2026-05-06 23:47:25 +0300

    coffeestyle v2.2

--- src/components/Home/Sub/Sub.jsx
+++ src/components/Home/Sub/Sub.jsx
@@ -6,9 +6,7 @@ import "./SubResponsive.css";
 const Sub = () => {
 	return (
 		<section className="sub">                                                                     
-			<div className="container">
-				<h1>Section Sub</h1>
-			</div>
+			
 		</section>
 	);
 };
