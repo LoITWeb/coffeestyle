662f40c6132743b3cb43569342f6c9ebe354fc5b
Andrey Lo <loandrey7@gmail.com>
2026-05-07 23:14:44 +0300

    coffeestyle v2.6

--- src/components/Home/Stories/Stories.jsx
+++ src/components/Home/Stories/Stories.jsx
@@ -13,33 +13,39 @@ const Stories = () => {
 				<div className="stories-content">
 					<div className="stiries-block">
 						<img src={stories_1} alt="" className="stories-img" />
-						<p className="stories-top-text">
-							Health Check: why do I get a headache when I haven’t had my coffee?
-						</p>
-						<p className="stories-sub-text">
-							It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-						</p>
-						<p className="stories-data">October 9, 2018</p>
+						<div className="stories-content-block">
+							<p className="stories-top-text">
+								Health Check: why do I get a headache when I haven’t had my coffee?
+							</p>
+							<p className="stories-sub-text">
+								It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+							</p>
+							<p className="stories-data">October 9, 2018</p>
+						</div>
 					</div>
 					<div className="stiries-block">
 						<img src={stories_2} alt="" className="stories-img" />
-						<p className="stories-top-text">
-							How long does a cup of coffee keep you awake?
-						</p>
-						<p className="stories-sub-text">
-							It is a paradisematic country, in which roasted parts. Vel qui et ad voluptatem.
-						</p>
-						<p className="stories-data">October 9, 2018</p>
+						<div className="stories-content-block">
+							<p className="stories-top-text">
+								How long does a cup of coffee keep you awake?
+							</p>
+							<p className="stories-sub-text">
+								It is a paradisematic country, in which roasted parts. Vel qui et ad voluptatem.
+							</p>
+							<p className="stories-data">October 9, 2018</p>
+						</div>
 					</div>
 					<div className="stiries-block">
 						<img src={stories_3} alt="" className="stories-img" />
-						<p className="stories-top-text">
-							Recent research suggests that heavy coffee drinkers may reap health benefits.
-						</p>
-						<p className="stories-sub-text">
-							It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-						</p>
-						<p className="stories-data">October 9, 2018</p>
+						<div className="stories-content-block">
+							<p className="stories-top-text">
+								Recent research suggests that heavy coffee drinkers may reap health benefits.
+							</p>
+							<p className="stories-sub-text">
+								It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+							</p>
+							<p className="stories-data">October 9, 2018</p>
+						</div>
 					</div>
 				</div>
 			</div>
